7f1bed5642139691dd77e8b2e955452377c4a13b
bolonge <xxs456@naver.com>
2019-10-14 22:44:06 +0900

    env수정

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
+console.log(process.env.PORT);
 
 const server = new GraphQLServer({ schema });
 
