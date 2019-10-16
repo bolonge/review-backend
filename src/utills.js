4a7fd1d0f73d467fe7ded6337def8012c5c76bd1
bolonge <xxs456@naver.com>
2019-10-16 22:20:28 +0900

    이메일 보내기

--- src/utills.js
+++ src/utills.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcryptjs";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const secretThings = async () => {
   const randomNumber = Math.floor(Math.random() * 99999);
@@ -6,6 +8,23 @@ export const secretThings = async () => {
   return `${randomNumber} ${randomHash}`;
 };
 
-export const sendMail = email => {};
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
 
-export const sendSecretMail = () => {};
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "bolong@reviewmaster.com",
+    to: adress,
+    subject: "리뷰 로그인 시크릿",
+    html: `로그인 시크릿 <br/>${secret}`
+  };
+  return sendMail(email);
+};
