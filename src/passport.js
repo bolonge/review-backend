07c0a87f13552b1162f8bc331b97eaf9300dc9ea
bolonge <xxs456@naver.com>
2019-10-18 00:12:00 +0900

    패스포트 jwt 불러오기

--- src/passport.js
+++ src/passport.js
@@ -1,6 +1,23 @@
 import passport from "passport";
 import JwtStractegy from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-  jwtFromRequest: JwtStractegy.ExtractJwt.fromAuthHeaderAsBearerToken()
+  jwtFromRequest: JwtStractegy.ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secret: process.env.JWT_SECRET
 };
+
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user({ id: payload.id });
+    if (user !== null) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (e) {
+    return done(e, false);
+  }
+};
+
+passport.use(new JwtStractegy(jwtOptions, verifyUser));
