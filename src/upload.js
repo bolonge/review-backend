6b711c55ad8c6546f3969f1d59c3489e7526913c
bolonge <xxs456@naver.com>
2019-10-18 21:32:46 +0900

    사진업로드 부분 주석처리

--- src/upload.js
+++ src/upload.js
@@ -2,17 +2,17 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_SECRET,
-  region: "ap-northeast-2"
-});
+// const s3 = new aws.S3({
+//   accessKeyId: process.env.AWS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET,
+//   region: "ap-northeast-2"
+// });
 
-const upload = multer({ storage: multerS3() });
+// const upload = multer({ storage: multerS3(s3) });
 
-export const uploadMiddleware = upload.single("file");
+// export const uploadMiddleware = upload.single("file");
 
-export const uploadController = (req, res) => {
-  const { file } = req;
-  res.end();
-};
+// export const uploadController = (req, res) => {
+//   const { file } = req;
+//   res.end();
+// };
