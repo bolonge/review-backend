bc2d3edbfff26bf272140ddf7fe5e0e3bf0eb7ae
bolonge <xxs456@naver.com>
2019-10-28 11:59:17 +0900

    리뷰수정코드 수정

--- src/api/Review/editReview/editReview.js
+++ src/api/Review/editReview/editReview.js
@@ -1,14 +1,30 @@
 import { prisma } from "../../../../generated/prisma-client";
 
+const DELETE = "DELETE";
+const EDIT = "EDIT";
+
 export default {
   Mutation: {
     editReview: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      const { text, rating, reviewId } = args;
-      return await prisma.updateReview({
-        where: { id: reviewId },
-        data: { text, rating }
+      const { text, rating, reviewId, action } = args;
+      const { user } = request;
+      const review = await prisma.$exists.review({
+        id: reviewId,
+        user: { id: user.id }
       });
+      if (review) {
+        if (action === EDIT) {
+          return await prisma.updateReview({
+            where: { id: reviewId },
+            data: { text, rating }
+          });
+        } else if (action === DELETE) {
+          return await prisma.deleteReview({ id: reviewId });
+        }
+      } else {
+        throw Error("권한이없습니다");
+      }
     }
   }
 };
