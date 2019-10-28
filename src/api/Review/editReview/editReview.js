86cc8968cc26ad7879740ff4c1f761a4022578c4
bolonge <xxs456@naver.com>
2019-10-28 11:44:37 +0900

    코드수정

--- src/api/Review/editReview/editReview.js
+++ src/api/Review/editReview/editReview.js
@@ -5,22 +5,6 @@ export default {
     editReview: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { text, rating, reviewId } = args;
-      const { user } = request;
-      const myReview = await prisma.$exists.user({
-        id: user.id,
-        myReview_some: { id: reviewId }
-      });
-      if (!myReview) {
-        throw Error("나의 리뷰가아니다");
-      }
-
-      const myReviewLike = await prisma
-        .likesConnection({ where: { review: { id: reviewId } } })
-        .aggregate()
-        .count();
-      if (myReviewLike > 5) {
-        throw Error("수정 x");
-      }
       return await prisma.updateReview({
         where: { id: reviewId },
         data: { text, rating }
