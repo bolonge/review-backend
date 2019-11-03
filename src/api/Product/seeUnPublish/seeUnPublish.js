2610770cc0b28dcbb9713cecdf25277325e9ab2c
bolonge <xxs456@naver.com>
2019-11-03 21:31:40 +0900

    발행되지않은 상품보기

---
+++ src/api/Product/seeUnPublish/seeUnPublish.js
@@ -0,0 +1,8 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeUnPublish: async (_, __) =>
+      await prisma.products({ where: { isPublished: false } })
+  }
+};
