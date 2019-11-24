ffe0649902f1c6dffaae9249c508f6e2b08e6112
bolonge <xxs456@naver.com>
2019-11-24 15:43:05 +0900

    회원가입수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { nickName, phone, email } = args;
+      const { nickName, phone, email, password } = args;
       const exists = await prisma.$exists.user({
         OR: [
           {
@@ -12,17 +12,18 @@ export default {
           { email }
         ]
       });
+      const blackCheck = await prisma.$exists.blackList({ email });
       if (exists) {
         throw Error("This nickName / email is already taken");
       }
-      const blackCheck = await prisma.$exists.blackList({ email });
       if (blackCheck) {
         throw Error("블랙리스트 회원");
       }
       await prisma.createUser({
         nickName,
         phone,
-        email
+        email,
+        password
       });
       return true;
     }
