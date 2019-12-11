026ceb1b772f9894034f79c9b1741b2f0213288f
bolonge <xxs456@naver.com>
2019-12-11 21:13:26 +0900

    product 발행 수정

--- src/api/Product/publishProduct/publishProduct.js
+++ src/api/Product/publishProduct/publishProduct.js
@@ -2,32 +2,13 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    publishProduct: async (_, args, { request, isAuthenticated }) => {
-      isAuthenticated(request);
+    publishProduct: async (_, args) => {
       const { id } = args;
-      const { user } = request;
-      const admin = await prisma.user({ id: user.id }).admin();
-      const publishCheck = await prisma.products({
-        where: {
-          AND: [{ id }, { OR: [{ isPublished: false }, { isPublished: true }] }]
-        }
+      await prisma.updateProduct({
+        where: { id },
+        data: { isPublished: true }
       });
-      if (admin) {
-        if (publishCheck[0].isPublished === false) {
-          await prisma.updateProduct({
-            where: { id },
-            data: { isPublished: true }
-          });
-        } else {
-          await prisma.updateProduct({
-            where: { id },
-            data: { isPublished: false }
-          });
-        }
-        return true;
-      } else {
-        return false;
-      }
+      return true;
     }
   }
 };
