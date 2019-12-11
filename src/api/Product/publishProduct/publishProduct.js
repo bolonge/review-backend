2bdb5cb4bf2fe8b85690e49b66f99ae2b3352a75
bolonge <xxs456@naver.com>
2019-12-11 21:18:04 +0900

    수정

--- src/api/Product/publishProduct/publishProduct.js
+++ src/api/Product/publishProduct/publishProduct.js
@@ -3,9 +3,9 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     publishProduct: async (_, args) => {
-      const { id } = args;
+      const { postId } = args;
       await prisma.updateProduct({
-        where: { id },
+        where: { id: postId },
         data: { isPublished: true }
       });
       return true;
