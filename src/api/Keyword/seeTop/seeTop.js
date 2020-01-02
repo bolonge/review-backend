9b37af0cea027aaffdcec69cba859ef1a859edf8
bolonge <xxs456@naver.com>
2020-01-02 21:52:19 +0900

    top10까지 출력

--- src/api/Keyword/seeTop/seeTop.js
+++ src/api/Keyword/seeTop/seeTop.js
@@ -3,10 +3,11 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeTop: async () => {
-      return await prisma.keywords({
+      const allKeyword = await prisma.keywords({
         where: { count_gt: 1 },
         orderBy: "count_DESC"
       });
+      return allKeyword.splice(0, 10);
     }
   }
 };
