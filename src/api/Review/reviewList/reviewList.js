55fcb10c7ef7b7f42461edb6dfbec309189a0438
bolonge <xxs456@naver.com>
2020-02-27 22:48:42 +0900

    리뷰리스트 수정

--- src/api/Review/reviewList/reviewList.js
+++ src/api/Review/reviewList/reviewList.js
@@ -2,6 +2,9 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    reviewList: async (_, __) => await prisma.reviews({})
+    reviewList: async (_, args) => {
+      const { id } = args;
+      return await prisma.reviews({ where: { id } });
+    }
   }
 };
