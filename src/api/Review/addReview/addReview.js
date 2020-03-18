53a820f0cfdc5b96b507dece87c51a689054f1aa
bolonge <xxs456@naver.com>
2020-03-18 22:13:27 +0900

    리뷰올리기수정

--- src/api/Review/addReview/addReview.js
+++ src/api/Review/addReview/addReview.js
@@ -6,30 +6,45 @@ export default {
       isAuthenticated(request);
       const { title, text, rating, productId, photos } = args;
       const { user } = request;
-      const review = await prisma.createReview({
-        title,
-        user: {
-          connect: { id: user.id }
-        },
-        product: {
-          connect: {
-            id: productId
-          }
-        },
-        rating,
-        text
-      });
-      photos.forEach(
-        async photo =>
-          await prisma.createPhoto({
-            url: photo,
-            review: {
-              connect: {
-                id: review.id
-              }
+      if (title !== "" && photos !== "") {
+        const review = await prisma.createReview({
+          title,
+          user: {
+            connect: { id: user.id }
+          },
+          product: {
+            connect: {
+              id: productId
             }
-          })
-      );
+          },
+          rating,
+          text
+        });
+        photos.forEach(
+          async photo =>
+            await prisma.createPhoto({
+              url: photo,
+              review: {
+                connect: {
+                  id: review.id
+                }
+              }
+            })
+        );
+      } else {
+        const review = await prisma.createReview({
+          user: {
+            connect: { id: user.id }
+          },
+          product: {
+            connect: {
+              id: productId
+            }
+          },
+          rating,
+          text
+        });
+      }
       return review;
     }
   }
