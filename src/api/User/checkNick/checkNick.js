6d7353591a8c886291b8c55f3e721d6f27e374a3
bolonge <xxs456@naver.com>
2020-04-07 23:02:01 +0900

    닉네임 수정

--- src/api/User/checkNick/checkNick.js
+++ src/api/User/checkNick/checkNick.js
@@ -2,9 +2,15 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    checkNick: async (_, args) => {
+    checkNick: async (_, args, { request }) => {
       const { nickName } = args;
-      return await prisma.$exists.user({ nickName });
+      const { user } = request;
+      const myName = await prisma.user({ id: user.id }).nickName();
+      if (myName === nickName) {
+        return false;
+      } else {
+        return await prisma.$exists.user({ nickName });
+      }
     }
   }
 };
