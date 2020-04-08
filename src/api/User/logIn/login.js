6bb3105511cc4462905dddaaaac21f60807cd40c
bolonge <xxs456@naver.com>
2020-04-08 21:52:05 +0900

    login update

--- src/api/User/logIn/login.js
+++ src/api/User/logIn/login.js
@@ -5,16 +5,28 @@ import * as bcrypt from "bcryptjs";
 export default {
   Mutation: {
     login: async (_, args) => {
-      const { nickName, password } = args;
-      const user = await prisma.user({ nickName });
-      if (!user) {
-        throw Error("이메일이 없습니다");
+      const { NameOrEmail, password, type } = args;
+      if (type === "EMAIL") {
+        const user = await prisma.user({ email: NameOrEmail });
+        if (!user) {
+          throw Error("이메일이 없습니다");
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+          throw Error("비밀번호 오류");
+        }
+        return generateToken(user.id);
+      } else if (type === "NICKNAME") {
+        const user = await prisma.user({ nickName: NameOrEmail });
+        if (!user) {
+          throw Error("이메일이 없습니다");
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+          throw Error("비밀번호 오류");
+        }
+        return generateToken(user.id);
       }
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        throw Error("비밀번호 오류");
-      }
-      return generateToken(user.id);
     }
   }
 };
