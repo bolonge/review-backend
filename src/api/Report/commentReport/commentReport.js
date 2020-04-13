b105fc931a06a39a35c0540e65fe4cb97e3af31e
bolonge <xxs456@naver.com>
2020-04-13 20:53:38 +0900

    오류수정

--- src/api/Report/commentReport/commentReport.js
+++ src/api/Report/commentReport/commentReport.js
@@ -2,14 +2,21 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    commentReport: async (_, args) => {
+    commentReport: async (_, args, { request }) => {
       const { commentId, why } = args;
+      const { user } = request;
+      const exists = await prisma.$exists.comment({ id: commentId });
       try {
-        await prisma.createReport({
-          why,
-          comment: { connect: { id: commentId } },
-        });
-        return true;
+        if (exists) {
+          await prisma.createReport({
+            why,
+            user: { connect: { id: user.id } },
+            comment: { connect: { id: commentId } },
+          });
+          return true;
+        } else {
+          return false;
+        }
       } catch (error) {
         console.log(error);
         return false;
--- src/api/Report/reviewReport/reviewReport.js
+++ src/api/Report/reviewReport/reviewReport.js
@@ -2,14 +2,22 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    commentReport: async (_, args) => {
+    reviewReport: async (_, args, { request }) => {
       const { reviewId, why } = args;
+      const { user } = request;
+      const exists = await prisma.$exists.review({ id: reviewId });
       try {
-        await prisma.createReport({
-          why,
-          review: { connect: { id: reviewId } },
-        });
-        return true;
+        if (exists) {
+          await prisma.createReport({
+            why,
+            user: { connect: { id: user.id } },
+            review: { connect: { id: reviewId } },
+          });
+          return true;
+        } else {
+          console.log("존재하지않습니다");
+          return false;
+        }
       } catch (error) {
         console.log(error);
         return false;
