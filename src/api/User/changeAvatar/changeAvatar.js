ce4d0c41d6c6e5f494a07509b3971fe53f47bea8
bolonge <xxs456@naver.com>
2020-04-13 23:57:08 +0900

    오류수정

--- src/api/User/changeAvatar/changeAvatar.js
+++ src/api/User/changeAvatar/changeAvatar.js
@@ -3,18 +3,17 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    changeAvatar: async (_, args, { request }) => {
+    changeAvatar: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { avatar } = args;
       const { user } = request;
       try {
         await prisma.updateUser({ where: { id: user.id }, data: { avatar } });
-
         return true;
       } catch (error) {
         console.log(error);
         return false;
       }
-    }
-  }
+    },
+  },
 };
