df84d51e573019c260a8bcaf93c8f28c97c7f925
bolonge <xxs456@naver.com>
2020-04-19 21:05:53 +0900

    검색추가

--- src/api/Product/searchProduct/searchProduct.js
+++ src/api/Product/searchProduct/searchProduct.js
@@ -2,25 +2,52 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    searchProduct: async (_, args) =>
-      await prisma.products({
+    searchProduct: async (_, args) => {
+      const products = await prisma.products({
         where: {
-          AND: [
-            { isPublished: true },
-            { productName_contains: args.term },
-            { productName_starts_with: args.term },
-            {
-              category: {
-                categoryName_contains: args.term
-              }
-            },
-            {
-              category: {
-                superCategory: { superCategoryName_contains: args.term }
-              }
-            }
-          ]
+          AND: [{ isPublished: true }, { productName_contains: args.term }]
         }
-      })
+      });
+      if (products) {
+        return (
+          products,
+          await prisma.products({
+            where: {
+              OR: [
+                { productName_starts_with: args.term },
+                {
+                  category: {
+                    categoryName_contains: args.term
+                  }
+                },
+                {
+                  category: {
+                    superCategory: { superCategoryName_contains: args.term }
+                  }
+                }
+              ]
+            }
+          })
+        );
+      } else {
+        await prisma.products({
+          where: {
+            OR: [
+              { productName_starts_with: args.term },
+              {
+                category: {
+                  categoryName_contains: args.term
+                }
+              },
+              {
+                category: {
+                  superCategory: { superCategoryName_contains: args.term }
+                }
+              }
+            ]
+          }
+        });
+      }
+    }
   }
 };
