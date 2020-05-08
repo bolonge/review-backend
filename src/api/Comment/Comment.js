c6d1082be903340a166a827d0f319eb54f45000e
bolonge <xxs456@naver.com>
2020-05-08 16:12:26 +0900

    수정

--- src/api/Comment/Comment.js
+++ src/api/Comment/Comment.js
@@ -3,6 +3,6 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Comment: {
     user: ({ id }) => prisma.comment({ id }).user(),
-    review: ({ id }) => prisma.comment({ id }).review()
-  }
+    review: ({ id }) => prisma.comment({ id }).review(),
+  },
 };
