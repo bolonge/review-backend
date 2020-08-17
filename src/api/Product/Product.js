6bdd8879c23fed6aa7c3e9160c7e2dbfdf5e4140
bolonge <xxs456@naver.com>
2020-08-17 23:53:12 +0900

    상품사진 한개만

--- src/api/Product/Product.js
+++ src/api/Product/Product.js
@@ -3,13 +3,13 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Product: {
     user: ({ id }) => prisma.product({ id }).user(),
-    productPhotos: ({ id }) => prisma.product({ id }).productPhotos(),
+    productPhoto: ({ id }) => prisma.product({ id }).productPhoto(),
     reviews: ({ id }) => prisma.product({ id }).reviews(),
     category: ({ id }) => prisma.product({ id }).category(),
-    reviewCount: parent =>
+    reviewCount: (parent) =>
       prisma
         .reviewsConnection({ where: { product: { id: parent.id } } })
         .aggregate()
-        .count()
-  }
+        .count(),
+  },
 };
